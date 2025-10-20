443b3dfb645b5f297752f693bc67b1c520e52951
魏子轩 <2629230038@qq.com>
2025-10-20 17:01:22 +0800

    feat: add customTag 组件

---
+++ components/customTag.vue
@@ -0,0 +1,38 @@
+<template>
+  <view class="custom-tag" :class="{ active: isActive }">
+    <text>{{ text }}</text>
+  </view>
+</template>
+<script>
+export default {
+  name: "customTag",
+  props: {
+    text: {
+      type: String,
+      default: "标签",
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+<style scoped>
+.custom-tag {
+  display: inline-block;
+  padding: 10rpx 20rpx;
+  border-radius: 30rpx;
+  font-size: 30rpx;
+  color: #555555;
+  background-color: #ffffff;
+  margin-right: 15rpx;
+  //border: 1rpx solid #ccc;
+  white-space: nowrap;
+}
+.custom-tag.active {
+  color: #ffffff;
+  background-color: #87653a;
+}
+</style>
