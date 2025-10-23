9e0035a85eb7c81dd4986346402727bfe30aafb9
wenting <792680697@qq.com>
2025-10-23 09:27:01 +0800

    add api

--- api/shopApi.js
+++ api/shopApi.js
@@ -31,6 +31,39 @@ const shop = {
 			method: "post",
 		},2)
 		return res
+	},
+	//获取商品详情
+	async getDetail(id) {
+		const res = await cjRequest({
+			url: "api/goods/detail?id="+id,
+			method: "get",
+		},2)
+		return res
+	},
+	//收货地址
+	async getAddressList(userId) {
+		const res = await cjRequest({
+			url: "api/address/list?userId="+userId,
+			method: "get"
+		},2)
+		return res
+	},
+	//更新、添加收货地址
+	async getAddressList(data) {
+		const res = await cjRequest({
+			url: "api/address/save?userId="+userId,
+			method: "post",
+		},2)
+		return res
+	},
+	
+	//删除收货地址
+	async getAddressList(data) {
+		const res = await cjRequest({
+			url: "api/address/delete?userId="+userId,
+			method: "post",
+		},2)
+		return res
 	}
 	
 }
