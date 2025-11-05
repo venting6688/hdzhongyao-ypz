fd5ca5662cdfafb2e2319e094df7865f31d6643e
魏子轩 <2629230038@qq.com>
2025-11-05 15:39:41 +0800

    优化订单项组件，调整退款原因输入样式和支付流程

--- sub_packages/shop/components/order-item.vue
+++ sub_packages/shop/components/order-item.vue
@@ -71,7 +71,12 @@
         </view>
         <view class="refund-reason">
           <text>退款原因：</text>
-          <textarea v-model="orderData.note" placeholder="请输入您的退款原因" maxLength="100" class="note-textarea"></textarea>
+          <textarea
+            v-model="orderData.note"
+            placeholder="请输入您的退款原因"
+            maxLength="100"
+            class="note-textarea"
+          ></textarea>
         </view>
       </view>
       <view class="popup-footer">
@@ -82,309 +87,311 @@
 </template>
 
 <script>
-  import shopApi from '@/api/shopApi.js'
-  import registrationApi from '@/api/registrationApi'
+import shopApi from '@/api/shopApi.js'
+import registrationApi from '@/api/registrationApi'
 
-  export default {
-    props: {
-      order: Object,
-      isShowFooter: {
-        type: Boolean,
-        default: true
+export default {
+  props: {
+    order: Object,
+    isShowFooter: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data() {
+    return {
+      loginData: null
+    }
+  },
+  filters: {
+    formatPrice(value) {
+      if (typeof value !== 'number') return '0.00'
+      return (value / 100).toFixed(2)
+    }
+  },
+  mounted() {
+    this.loginData = uni.getStorageSync('loginData')
+  },
+  methods: {
+    viewLogistics(order) {
+      console.log('查看物流', order.id)
+    },
+    async confirmOrder(order) {
+      const res = await shopApi.confirmOrderApi({
+        orderId: order.id,
+        userId: this.loginData.userId
+      })
+      if (res.errno === 0) {
+        uni.showToast({
+          title: '确认收货成功',
+          icon: 'success'
+        })
+        this.$emit('getOrderListEmit')
       }
     },
-    data() {
-      return {
-        loginData: null
+    async cancelOrder(order) {
+      const res = await shopApi.cancelOrderApi({
+        orderId: order.id,
+        userId: this.loginData.userId
+      })
+      if (res.errno === 0) {
+        uni.showToast({
+          title: '取消成功',
+          icon: 'success'
+        })
+        this.$emit('getOrderListEmit')
       }
     },
-    filters: {
-      formatPrice(value) {
-        if (typeof value !== 'number') return '0.00'
-        return (value / 100).toFixed(2)
+    async payNow(orderInfo) {
+      const res = await shopApi.getRowDataApi({
+        merOrderId: orderInfo?.orderSn,
+        userId: this.loginData.userId
+      })
+      if (res.code == 200) {
+        const payParams = JSON.parse(res.data.rowData);
+        if (payParams && "miniPayRequest" in payParams) {
+          this.startPayment(payParams.miniPayRequest, orderInfo)
+        }
+      } else {
+        uni.showToast({
+          title: '获取支付参数失败',
+          icon: 'none'
+        })
       }
     },
-    mounted() {
-      this.loginData = uni.getStorageSync('loginData')
+    /** 启动支付流程 */
+    startPayment(payParams, { id, orderSn }) {
+      if (!payParams || !orderSn) throw new Error('缺少支付参数')
+      uni.requestPayment({
+        provider: 'wxpay', // 服务提提供商
+        timeStamp: payParams.timeStamp, // 时间戳
+        nonceStr: payParams.nonceStr, // 随机字符串
+        package: payParams.package,
+        signType: payParams.signType, // 签名算法
+        paySign: payParams.paySign, // 签名
+        success: result => {
+          uni.showToast({
+            title: '支付成功',
+            icon: 'success'
+          })
+          shopApi.updateSuccessApi({
+            orderId: id,
+            orderSn: orderSn,
+            userId: this.loginData.userId
+          }).then(res => {
+            if (res.errno === 0) {
+              this.$emit('getOrderListEmit')
+            }
+          })
+        },
+        fail: result => {
+          uni.showToast({
+            title: '支付失败',
+            icon: 'none'
+          })
+        }
+      })
     },
-    methods: {
-      viewLogistics(order) {
-        console.log('查看物流', order.id)
-      },
-      async confirmOrder(order) {
-        const res = await shopApi.confirmOrderApi({
-          orderId: order.id,
-          userId: this.loginData.userId
+    buyAgain(order) {
+      console.log('再次购买', order.id)
+    },
+    onClickOrderDetail(order) {
+      uni.navigateTo({
+        url: '/sub_packages/shop/orderDetail?id=' + order.id
+      })
+    },
+    async onClickApplyRefund(order) {
+      this.$refs.refundPopup.open()
+    },
+    closeNotePopup() {
+      this.$refs.refundPopup.close()
+    },
+    async onClickConfirm() {
+      const res = await shopApi.applyRefundApi({
+        orderId: this.order.id,
+        userId: this.loginData.userId,
+        note: this.orderData.note
+      })
+      if (res.errno === 0) {
+        uni.showToast({
+          title: '申请退款成功',
+          icon: 'success'
         })
-        if (res.errno === 0) {
-          uni.showToast({
-            title: '确认收货成功',
-            icon: 'success'
-          })
-          this.$emit('getOrderListEmit')
-        }
-      },
-      async cancelOrder(order) {
-        const res = await shopApi.cancelOrderApi({
-          orderId: order.id,
-          userId: this.loginData.userId
+        this.$emit('getOrderListEmit')
+        this.closeNotePopup()
+      } else {
+        uni.showToast({
+          title: res.errmsg || '申请退款失败',
+          icon: 'error'
         })
-        if (res.errno === 0) {
-          uni.showToast({
-            title: '取消成功',
-            icon: 'success'
-          })
-          this.$emit('getOrderListEmit')
-        }
-      },
-      async payNow(orderInfo) {
-        const datas = {
-          lockId: '',
-          patientId: this.loginData.defaultArchives?.idNum,
-          patientName: this.loginData.defaultArchives?.patientName,
-          subOpenId: this.loginData?.xcxOpenId,
-          totalAmount: String(orderInfo?.total),
-          merOrderId: orderInfo?.orderSn,
-          uploadData: {},
-          payType: 'shopPay'
-        }
-        const resRegister = await registrationApi.againOrderApi(datas)
-        this.startPayment(resRegister, orderInfo)
-      },
-      /** 启动支付流程 */
-      startPayment({
-        data
-      }, {
-        id,
-        orderSn
-      }) {
-        if (!data?.miniPayRequest || !orderSn) throw new Error('缺少支付参数')
-        uni.requestPayment({
-          provider: 'wxpay', // 服务提提供商
-          timeStamp: data.miniPayRequest.timeStamp, // 时间戳
-          nonceStr: data.miniPayRequest.nonceStr, // 随机字符串
-          package: data.miniPayRequest.package,
-          signType: data.miniPayRequest.signType, // 签名算法
-          paySign: data.miniPayRequest.paySign, // 签名
-          success: result => {
-            uni.showToast({
-              title: '支付成功',
-              icon: 'success'
-            })
-            shopApi.updateSuccessApi({
-              orderId: id,
-              orderSn: orderSn,
-              userId: this.loginData.userId
-            })
-          },
-          fail: result => {
-            uni.showToast({
-              title: '支付失败',
-              icon: 'none'
-            })
-          }
-        })
-      },
-      buyAgain(order) {
-        console.log('再次购买', order.id)
-      },
-      onClickOrderDetail(order) {
-        uni.navigateTo({
-          url: '/sub_packages/shop/orderDetail?id=' + order.id
-        })
-      },
-      async onClickApplyRefund(order) {
-        this.$refs.refundPopup.open()
-      },
-      closeNotePopup() {
-        this.$refs.refundPopup.close()
-      },
-      async onClickConfirm() {
-        const res = await shopApi.applyRefundApi({
-          orderId: this.order.id,
-          userId: this.loginData.userId,
-          note: this.orderData.note
-        })
-        if (res.errno === 0) {
-          uni.showToast({
-            title: '申请退款成功',
-            icon: 'success'
-          })
-          this.$emit('getOrderListEmit')
-          this.closeNotePopup()
-        } else {
-          uni.showToast({
-            title: res.errmsg || '申请退款失败',
-            icon: 'error'
-          })
-        }
       }
     }
   }
+}
 </script>
 
 <style scoped lang="less">
-  .order-card {
-    background: #fff;
-    border-radius: 12rpx;
-    margin-bottom: 20rpx;
+.order-card {
+  background: #fff;
+  border-radius: 12rpx;
+  margin-bottom: 20rpx;
+}
+
+.order-card.has-padding {
+  padding: 35rpx;
+}
+
+.order-header {
+  display: flex;
+  justify-content: space-between;
+  color: #666;
+  font-size: 26rpx;
+  margin-bottom: 20rpx;
+}
+
+.order-status {
+  color: #faaa03;
+}
+
+.order-body {
+  display: flex;
+  margin-top: 15rpx;
+}
+
+.order-img {
+  width: 120rpx;
+  height: 120rpx;
+  border-radius: 8rpx;
+  margin-right: 20rpx;
+}
+
+.order-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: 1;
+  padding: 15rpx 15rpx 15rpx 0;
+}
+
+.price-qty {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.order-price {
+  color: #333;
+  font-size: 28rpx;
+}
+
+.order-qty {
+  color: #999;
+  font-size: 26rpx;
+}
+
+.order-footer {
+  border-top: 1px solid #eee;
+  margin-top: 20rpx;
+  padding-top: 20rpx;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.total {
+  color: #333;
+  font-weight: bold;
+}
+
+.actions {
+  display: flex;
+  gap: 20rpx;
+}
+
+.btn {
+  font-size: 26rpx;
+  padding: 8rpx 20rpx;
+  border: 1px solid #a77d50;
+  border-radius: 30rpx;
+  color: #a77d50;
+  background: transparent;
+}
+
+.btn.primary {
+  background: #a77d50;
+  color: #fff;
+}
+
+.popup-header {
+  display: flex;
+  justify-content: center;
+  /* 居中标题 */
+  align-items: center;
+  position: relative;
+  padding: 30rpx;
+  font-size: 32rpx;
+  font-weight: bold;
+}
+
+.popup-header uni-icons {
+  position: absolute;
+  right: 30rpx;
+  top: 30rpx;
+}
+
+.popup-title {
+  font-size: 36rpx;
+  font-weight: bold;
+  text-align: center;
+  color: #333;
+}
+
+.popup-content {
+  height: 770rpx;
+  overflow: auto;
+  padding: 0 30rpx;
+
+  .refund-amount-container {
+    margin: 25rpx 0;
+
+    .refund-amount {
+      font-size: 28rpx;
+      font-weight: bold;
+      color: #333;
+      float: right;
+    }
   }
 
-  .order-card.has-padding {
-    padding: 35rpx;
-  }
-
-  .order-header {
+  .refund-reason {
     display: flex;
-    justify-content: space-between;
-    color: #666;
-    font-size: 26rpx;
-    margin-bottom: 20rpx;
-  }
 
-  .order-status {
-    color: #faaa03;
+    > text {
+      width: 160rpx;
+    }
   }
+}
 
-  .order-body {
-    display: flex;
-    margin-top: 15rpx;
-  }
-
-  .order-img {
-    width: 120rpx;
-    height: 120rpx;
-    border-radius: 8rpx;
-    margin-right: 20rpx;
-  }
-
-  .order-info {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    flex: 1;
-    padding: 15rpx 15rpx 15rpx 0;
-  }
-
-  .price-qty {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .order-price {
-    color: #333;
-    font-size: 28rpx;
-  }
-
-  .order-qty {
-    color: #999;
-    font-size: 26rpx;
-  }
-
-  .order-footer {
-    border-top: 1px solid #eee;
-    margin-top: 20rpx;
-    padding-top: 20rpx;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .total {
-    color: #333;
-    font-weight: bold;
-  }
-
-  .actions {
-    display: flex;
-    gap: 20rpx;
-  }
-
-  .btn {
-    font-size: 26rpx;
-    padding: 8rpx 20rpx;
-    border: 1px solid #a77d50;
-    border-radius: 30rpx;
-    color: #a77d50;
-    background: transparent;
-  }
-
-  .btn.primary {
-    background: #a77d50;
+.popup-footer {
+  .confirm-btn {
+    background-color: #9a7546;
     color: #fff;
-  }
-
-  .popup-header {
-    display: flex;
-    justify-content: center;
-    /* 居中标题 */
-    align-items: center;
-    position: relative;
-    padding: 30rpx;
+    border: none;
+    border-radius: 50rpx;
     font-size: 32rpx;
-    font-weight: bold;
+    width: 95%;
+    margin-bottom: 20rpx;
   }
+}
 
-  .popup-header uni-icons {
-    position: absolute;
-    right: 30rpx;
-    top: 30rpx;
-  }
-
-  .popup-title {
-    font-size: 36rpx;
-    font-weight: bold;
-    text-align: center;
-    color: #333;
-  }
-
-  .popup-content {
-    height: 770rpx;
-    overflow: auto;
-    padding: 0 30rpx;
-
-    .refund-amount-container {
-      margin: 25rpx 0;
-
-      .refund-amount {
-        font-size: 28rpx;
-        font-weight: bold;
-        color: #333;
-        float: right;
-      }
-    }
-
-    .refund-reason {
-      display: flex;
-
-      >text {
-        width: 160rpx;
-      }
-    }
-  }
-
-  .popup-footer {
-    .confirm-btn {
-      background-color: #9a7546;
-      color: #fff;
-      border: none;
-      border-radius: 50rpx;
-      font-size: 32rpx;
-      width: 95%;
-      margin-bottom: 20rpx;
-    }
-  }
-
-  .note-textarea {
-    //width: 75%;
-    height: 250rpx;
-    padding: 20rpx;
-    //margin: 20rpx auto;
-    font-size: 28rpx;
-    border: 1rpx solid #ccc;
-    border-radius: 12rpx;
-    resize: none;
-    background-color: #f5f5f5;
-  }
+.note-textarea {
+  //width: 75%;
+  height: 250rpx;
+  padding: 20rpx;
+  //margin: 20rpx auto;
+  font-size: 28rpx;
+  border: 1rpx solid #ccc;
+  border-radius: 12rpx;
+  resize: none;
+  background-color: #f5f5f5;
+}
 </style>
