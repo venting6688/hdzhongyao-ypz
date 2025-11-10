c231e46ac8e7489b1e6939e406ee1a10548a71b5
wenting <792680697@qq.com>
2025-11-10 13:05:08 +0800

    modify ai

--- api/familyApi.js
+++ api/familyApi.js
@@ -1,73 +1,73 @@
-import { cjRequest } from "@/utils/service.js";
+import { cjRequest } from '@/utils/service.js'
 
 const family = {
-    // 查询
-    async getMemberListApi(params) {
-        const url = `member/list?ownerUserId=${params.ownerUserId}`;
-        const res = await cjRequest(
-            {
-                url: url,
-                method: "get",
-            },
-            2
-        );
-        return res?.data || {};
-    },
+  // 查询
+  async getMemberListApi(params) {
+    const url = `member/list?ownerUserId=${params.ownerUserId}`
+    const res = await cjRequest(
+      {
+        url: url,
+        method: 'get'
+      },
+      2
+    )
+    return res?.data || {}
+  },
 
-    // 新增家庭成员
-    async addMemberApi(params) {
-        const url = `member/create`;
-        const res = await cjRequest(
-            {
-                url: url,
-                method: "post",
-                data: params,
-            },
-            2
-        );
-        return res?.data || {};
-    },
+  // 新增家庭成员
+  async addMemberApi(params) {
+    const url = `member/create`
+    const res = await cjRequest(
+      {
+        url: url,
+        method: 'post',
+        data: params
+      },
+      2
+    )
+    return res?.data || {}
+  },
 
-    // 设置默认就诊人
-    async setDefaultMemberApi(params) {
-        const url = `member/setDefault/${params.familyId}/${params.ownerUserId}`;
-        const res = await cjRequest(
-            {
-                url: url,
-                method: "post",
-                data: params,
-            },
-            2
-        );
-        return res?.data || {};
-    },
+  // 设置默认就诊人
+  async setDefaultMemberApi(params) {
+    const url = `member/setDefault/${params.familyId}/${params.ownerUserId}`
+    const res = await cjRequest(
+      {
+        url: url,
+        method: 'post',
+        data: params
+      },
+      2
+    )
+    return res?.data || {}
+  },
 
-    // 查询默认就诊人
-    async getDefaultPatientApi(params) {
-        const url = `getDefautlPatient?userId=${params.ownerUserId}`;
-        const res = await cjRequest(
-            {
-                url: url,
-                method: "get",
-            },
-            2
-        );
-        return res?.data || {};
-    },
+  // 查询默认就诊人
+  async getDefaultPatientApi(params) {
+    const url = `getDefautlPatient?userId=${params.ownerUserId}`
+    const res = await cjRequest(
+      {
+        url: url,
+        method: 'get'
+      },
+      2
+    )
+    return res?.data || {}
+  },
 
-    // 删除某一就诊人
-    async deleteMemberApi(params) {
-        const url = `member/delete/${params.familyId}/${params.ownerUserId}`;
-        const res = await cjRequest(
-            {
-                url: url,
-                method: "delete",
-                data: params,
-            },
-            2
-        );
-        return res?.data || {};
-    },
-};
+  // 删除某一就诊人
+  async deleteMemberApi(params) {
+    const url = `member/delete/${params.familyId}/${params.ownerUserId}`
+    const res = await cjRequest(
+      {
+        url: url,
+        method: 'delete',
+        data: params
+      },
+      2
+    )
+    return res?.data || {}
+  }
+}
 
-export default family;
+export default family
